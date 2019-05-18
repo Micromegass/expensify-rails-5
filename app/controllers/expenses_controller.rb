95b7d103f57486e1555a83dd4f9c7b7651fc8440
axelbraunschweiger <axelbraunschweiger@gmail.com>
2019-05-18 14:14:07 -0500

    Start CRUD modal AJAX

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,4 +3,28 @@ class ExpensesController < ApplicationController
     # @tab = :expenses
     @expenses = Expense.all.order(date: :desc)
   end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.create(expense_params)
+  end
+
+  def edit
+    @expense = Expense.find(params[:id])
+  end
+
+  def update
+    @product = Expense.update(params[:id], expense_params)
+  end
+
+
+
+  private
+  def expense_params
+    params.require(:exense).permit(:description, :date, :type, :amount)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root "dashboard#index"
 
-  resources :expenses, only: [:index]
+  resources :expenses
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
 end
