a3957d1c4da6946fcf2e22b9f9ab695b0e750ea5
axelbraunschweiger <axelbraunschweiger@gmail.com>
2019-05-21 18:51:46 -0500

    Implement create view withou page reload

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -24,7 +24,7 @@ class ExpensesController < ApplicationController
 
   private
   def expense_params
-    params.require(:exense).permit(:description, :date, :type, :amount)
+    params.require(:expense).permit(:description, :date, :type, :amount, :id)
   end
 
 end
