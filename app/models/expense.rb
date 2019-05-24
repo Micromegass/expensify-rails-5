2f661d7e560e2a944b91ad3b13540b26185c80ca
axelbraunschweiger <axelbraunschweiger@gmail.com>
2019-05-23 19:05:18 -0500

    Write methods for calculation

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -18,3 +18,32 @@ class Expense < ApplicationRecord
   enum type: [:purchase, :payment, :transfer]
   enum category: [:education, :transport, :food, :alcohol, :rent]
 end
+
+
+def calculate_amount()
+  @expenses = Expense.all()
+  sum = 0
+    @expenses.each do |expense|
+      sum = sum + expense.amount
+    end
+  return sum
+end
+
+def calculate_transactions()
+  @expenses = Expense.all()
+  sum = 0
+    @expenses.each do |expense|
+      if expense.type == "transfer"
+        sum += 1
+      end
+    end
+  return sum
+end
+
+
+def calculate_average()
+  number_expenses = Expense.all().count()
+  amount = calculate_amount()
+  avg = amount/number_expenses
+  return avg
+end
